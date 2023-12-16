6fef40108f9bf775bb72f571b5a5c4bd115315e7
Umesh <umeshmane575@gmail.com>
2023-12-16 11:20:14 +0530

    i have created diffrent pages using react-router-dom to navigate

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useAuth } from "../context/auth";
+
+const Home = () => {
+  const [auth, setAuth] = useAuth();
+  return (
+    <div>
+       <h1 className="display-1 bg-primary text-light p-5">Home</h1>
+       <pre>{JSON.stringify(auth, null, 4)}</pre>
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+       <h1 className="display-1 bg-primary text-light p-5">Login</h1>
+    </div>
+  )
+}
+
+export default Login
