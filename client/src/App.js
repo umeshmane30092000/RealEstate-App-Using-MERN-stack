6ab8c0f884e1456b9a80251361b9344abeed5261
Umesh <umeshmane575@gmail.com>
2023-12-16 13:11:41 +0530

    componets nav is created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { AuthProvider } from "./context/auth";
+import Main from "./components/nav/Main";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -8,6 +9,7 @@ import Register from "./pages/Register";
 function App() {
   return (
     <BrowserRouter>
+      <Main />
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Home />} />
