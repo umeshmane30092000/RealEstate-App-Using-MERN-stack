d139558b5c9ea4b57cf09de9f072b59f6c5a605c
Umesh <umeshmane575@gmail.com>
2023-12-19 18:05:09 +0530

    we have created sidebar for dashboard it is protected  using privateroute

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,10 @@ import Register from "./pages/Register";
 import AccountActivate from "./pages/auth/AccountActivate";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import AccessAccount from "./pages/auth/AccessAccount";
+import Dashboard from "./pages/user/Dashboard";
+import AdCreate from "./pages/user/ad/AdCreate";
+import PrivateRoute from "./components/routes/PrivateRoute";
+
 
 function App() {
   return (
@@ -29,6 +33,10 @@ function App() {
             path="/auth/access-account/:token"
             element={<AccessAccount />}
           />
+         <Route path="/" element={<PrivateRoute />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="ad/create" element={<AdCreate />} />
+          </Route>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
