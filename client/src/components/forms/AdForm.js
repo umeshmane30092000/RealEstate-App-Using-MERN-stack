068fad29537da02e47a86bd5386b896430acd116
Umesh <umeshmane575@gmail.com>
2023-12-28 15:51:13 +0530

    form is created this is reusable component in the pages | 4- pages is created for sell - Rent \ House and land  Also imported in the app.js  routing is set working fine

---
+++ client/src/components/forms/AdForm.js
@@ -0,0 +1,8 @@
+export default function AdForm({action , type}) {
+    return(
+        <>
+        this is the form 
+        {action}/{type}
+        </>
+    );
+}
---
+++ client/src/pages/user/ad/RentHouse.js
@@ -0,0 +1,15 @@
+import Sidebar from "../../../components/nav/Sidebar";
+import AdForm from "../../../components/forms/AdForm";
+
+export default function RentHouse() {
+    return (
+        <div>
+            <h1 className="display-1 bg-primary text-light p-5">Rent House</h1>
+            <Sidebar />
+            <div>
+                <AdForm action="Rent" type="House" />
+            </div>
+        </div>
+
+    );
+}
